28ef7fc8c3b32b39635ee41ed4208ea94cff32bc
De-la-soul-is-dead <112073514+De-la-soul-is-dead@users.noreply.github.com>
2023-12-01 23:06:54 +0700

    Updarte

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,16 @@ Route::middleware('auth')->group(function () {
     Route::get('carts', [CartController::class, 'index'])->name('client.carts.index');
     Route::post('update-quantity-product-in-cart/{cart_product_id}', [CartController::class, 'updateQuantityProduct'])->name('client.carts.update_product_quantity');
     Route::post('remove-product-in-cart/{cart_product_id}', [CartController::class, 'removeProductInCart'])->name('client.carts.remove_product');
+
+    Route::post('apply-coupon', [CartController::class, 'applyCoupon'])->name('client.carts.apply_coupon');
+
+    Route::get('checkout', [CartController::class, 'checkout'])->name('client.checkout.index')->middleware('user.can_checkout_cart');
+    Route::post('process-checkout', [CartController::class, 'processCheckout'])->name('client.checkout.proccess')->middleware('user.can_checkout_cart');
+
+    Route::get('list-orders', [OrderController::class, 'index'])->name('client.orders.index');
+
+    Route::post('orders/cancel/{id}', [OrderController::class, 'cancel'])->name('client.orders.cancel');
+
 });
 
 
